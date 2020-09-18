b15af4e1d4af8ffee40f89a2154e5c6682a6d50c
djm160830 <djm160830@utdallas.edu>
2020-09-17 23:54:08 -0500

    Final commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Python version: Python 3.8.1
 
-Install dependences using command pip install -r requirements.txt
+Install dependences: pip install -r requirements.txt
+
+Run part 1: python part1.py
+Run part 2: python part2.py
+
+part 2 expected output: prediction for target variable y
+part 2 expected output: prediction for target variable y
--- part2.py
+++ part2.py
@@ -1,82 +1,13 @@
-# NAME:			Darla Maneja (djm160830@utdallas.edu)
-# SECTION:		CS4371.001
-# DATE:			Sept. 7, 2020
-# ASSIGNMENT 1
-
+import time
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
-import time
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.feature_selection import SelectKBest, f_classif
-
-np.random.seed(0)
-
-"""
-This class for creates a model for linear regression using the adam optimizer. 
-Methods: fit, predict, gradient_descent_adam
-"""
-class LinearRegression:
-	def __init__(self, hypothesis=None, theta=None, step_size=None):
-		self.hypothesis=hypothesis
-		self.theta=theta 
-		self.step_size=step_size
-
-
-	"""
-	Calculates the hypothesis (h). Compact representation shown in class was the dot product of theta (parameters) and X components.
-	"""
-	def fit(self, x, y):
-		self.theta = np.random.rand(x.shape[1]).reshape(-1, 1) 
-		x = np.asarray(x)
-		y = np.asarray(y)
-
-		self.hypothesis = self.gradient_descent_adam(x, y)
-
-	
-
-	"""
-	Calculates the optimal values for parameters (theta_0 and theta_1).
-	"""
-	def gradient_descent_adam(self, x, y):
-		# Recommended default values
-		self.step_size=0.01
-		epsilon=10**(-8)
-		beta_1=0.9
-		beta_2=0.999
-
-		# Moment vectors
-		m=0
-		v=0
-
-		# Time
-		t=x.shape[0]
-		tme = time.perf_counter()
-		i=0
-		time_cutoff=300.0
-
-		while time.perf_counter()-tme<=time_cutoff:
-			i+=1
-			self.hypothesis = np.dot(x, self.theta)
-			error = self.hypothesis - y
-			
-			mse = np.square(error).mean()
-			# print(f'Iteration {i}	|	MSE: {mse}	|	R^2: {r2_score(y, self.hypothesis)}	|	time: {time.perf_counter()-tme}')
-			partial = np.dot(x.T, error)/t
-			m = (1-beta_1)*partial + beta_1*m
-			v = (1-beta_2)*(partial**2) + beta_2*v
-			m_hat = m/(1-beta_1)
-			v_hat = v/(1-beta_2)
-			self.theta = self.theta - (self.step_size*m_hat)/(np.sqrt(v_hat)+epsilon)
-
-		return self.hypothesis
-
-	def predict(self, X):
-		return np.dot(X, self.theta)
-
+from sklearn.neural_network import MLPClassifier
 
 
 '''
@@ -158,14 +89,11 @@ if __name__=='__main__':
 	test_features = scaler.transform(test_features.values)
 	X_test_scaled[col_names] = test_features
 
-
 	# Construct model
-	model = LinearRegression()
-	model.fit(X_train_scaled, Y_train)
+	clf = MLPClassifier(random_state=1, solver='adam', verbose=True, learning_rate='adaptive', max_iter=50, learning_rate_init=0.001).fit(X_train, Y_train.values.ravel())
 
 	# Predict y
-	y_predict = model.predict(X_test_scaled)
+	y_predict = clf.predict(X_test_scaled).reshape(-1, 1)
 
-
-	print('\n\nY PREDICTION: {y_predict}\n')
-	print(f"\nError: {y_predict - Y_test} \n\nMSE: {np.square(y_predict - Y_test).mean()}\n\nR^2: {r2_score(Y_test, y_predict)}")
+	print(f"\n\nY PREDICTION: {y_predict}\n")
+	# print(f"\nError: {y_predict - Y_test} \n\nMSE: {np.square(y_predict - Y_test).mean()}\n\nR^2: {r2_score(Y_test, y_predict)}")
